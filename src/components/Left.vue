15ccfc063be14691105f81aa0b613eb1905bd717
Vivekanand P V <vivekanandpv@outlook.com>
2024-01-19 09:49:46 +0530

    done

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,3 +1,10 @@
 <template>
     <h3>Left Component</h3>
+    <p>Counter: {{ store.counter }}</p>
 </template>
+
+<script setup lang="ts">
+import { appStore } from '../store/app.store';
+
+const store = appStore();
+</script>
